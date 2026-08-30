fdf758d0f3c77de7151f7b844431ae7ef78c8969
avarap <avarap@gmail.com>
2026-08-30 08:05:16 +0200

    test: include npc navigation suite

--- tests/run_tests.gd
+++ tests/run_tests.gd
@@ -6,6 +6,7 @@ func _initialize() -> void:
 	_run_suite("TimeMath", TestTimeMath.run(), failures)
 	_run_suite("SimulationTime", TestSimulationTime.run(), failures)
 	_run_suite("DayNightCycle", TestDayNightCycle.run(), failures)
+	_run_suite("NPCNavigation", TestNPCNavigation.run(), failures)
 	_run_suite("PlayerMovement", TestPlayerMovement.run(), failures)
 	_run_suite("WalkingPrototype", TestWalkingPrototype.run(), failures)
 	_run_suite("InventoryModel", TestInventoryModel.run(), failures)
