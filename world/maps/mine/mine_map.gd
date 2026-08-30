aee637ddf95d6c0cc15238959f5f536df696d447
avarap <avarap@gmail.com>
2026-08-30 17:00:21 +0200

    fix: use technical map layer API in mine

--- world/maps/mine/mine_map.gd
+++ world/maps/mine/mine_map.gd
@@ -12,9 +12,9 @@ func _populate_mine_structure() -> void:
 	for y in range(5, 26):
 		if y in [10, 16, 22]:
 			continue
-		collision_layer.set_cell(Vector2i(divider_x, y), SOURCE_ID, ATLAS_BLOCK)
+		collision.set_cell(Vector2i(divider_x, y), SOURCE_ID, COLLISION_TILE)
 
 	for x in range(14, 21):
-		foreground_layer.set_cell(Vector2i(x, 8), SOURCE_ID, ATLAS_BLOCK)
+		foreground_occlusion.set_cell(Vector2i(x, 8), SOURCE_ID, FOREGROUND_TILE)
 	for x in range(31, 37):
-		foreground_layer.set_cell(Vector2i(x, 18), SOURCE_ID, ATLAS_BLOCK)
+		foreground_occlusion.set_cell(Vector2i(x, 18), SOURCE_ID, FOREGROUND_TILE)
