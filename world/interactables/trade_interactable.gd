b58007a7dcf37c54c4d84aa21d2db294636be6ee
avarap <avarap@gmail.com>
2026-08-30 12:41:14 +0200

    fix: localize trade interaction prompt

--- world/interactables/trade_interactable.gd
+++ world/interactables/trade_interactable.gd
@@ -1,6 +1,17 @@
 class_name TradeInteractable
 extends Interactable
 
+const PROMPT_KEY: StringName = &"UI_TRADE_PROMPT"
+
+
+func _ready() -> void:
+	_refresh_prompt()
+
+
+func _notification(what: int) -> void:
+	if what == NOTIFICATION_TRANSLATION_CHANGED and is_inside_tree():
+		_refresh_prompt()
+
 
 func _on_interact(actor: Node) -> void:
 	if not is_inside_tree():
@@ -12,3 +23,7 @@ func _on_interact(actor: Node) -> void:
 	if not trade_panel.has_method("open_trade"):
 		return
 	trade_panel.call("open_trade", actor, economy)
+
+
+func _refresh_prompt() -> void:
+	prompt = LocalizationService.translate_key(PROMPT_KEY)
