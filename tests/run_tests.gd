84d3bbe51bcefc910637db008a7524ed30793fe4
avarap <avarap@gmail.com>
2026-08-30 22:45:50 +0200

    test: register corpse decomposition suite

--- tests/run_tests.gd
+++ tests/run_tests.gd
@@ -29,6 +29,7 @@ func _run_all_tests() -> void:
 	_run_suite("StorageNetwork", TestStorageNetwork.run(), failures)
 	_run_suite("ProductionQueue", TestProductionQueue.run(), failures)
 	_run_suite("CemeteryFoundation", TestCemeteryFoundation.run(), failures)
+	_run_suite("CorpseDecomposition", TestCorpseDecomposition.run(), failures)
 	_run_suite("CemeteryFlow", TestCemeteryFlow.run(), failures)
 	_run_suite("CemeteryPersistence", TestCemeteryPersistence.run(), failures)
 	_run_suite("CemeteryGameplay", TestCemeteryGameplay.run(), failures)
