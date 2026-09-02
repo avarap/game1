6da8f1d984ce81e3f6063bac5f17385df474df40
avarap <avarap@gmail.com>
2026-09-02 08:44:25 +0200

    feat(player): expose explicit idle walk run visual state

--- player/player_visual.gd
+++ player/player_visual.gd
@@ -1,35 +1,31 @@
 class_name PlayerVisual
 extends AnimatedSprite2D
 
-const MOVE_EPSILON := 1.0
-const DIRECTION_NAMES: Array[StringName] = [
-	&"e",
-	&"se",
-	&"s",
-	&"sw",
-	&"w",
-	&"nw",
-	&"n",
-	&"ne",
-]
+const RUN_ANIMATION_SCALE := 1.35
 
 var _facing := &"s"
+var _locomotion_state := &"idle"
 
 
-func _process(_delta: float) -> void:
-	var actor := get_parent() as CharacterBody2D
-	if actor == null:
-		return
-	var moving := actor.velocity.length_squared() > MOVE_EPSILON
-	if moving:
-		_facing = _direction_name(actor.velocity)
-	var target := StringName("%s_%s" % ["walk" if moving else "idle", _facing])
+func set_locomotion_state(state: StringName, facing_vector: Vector2) -> void:
+	_locomotion_state = state
+	if not facing_vector.is_zero_approx():
+		_facing = PlayerMovement.direction_name(facing_vector)
+	_apply_animation()
+
+
+func get_facing_name() -> StringName:
+	return _facing
+
+
+func get_locomotion_state() -> StringName:
+	return _locomotion_state
+
+
+func _apply_animation() -> void:
+	var animation_state := &"walk" if _locomotion_state == &"run" else _locomotion_state
+	var target := StringName("%s_%s" % [animation_state, _facing])
+	var target_speed_scale := RUN_ANIMATION_SCALE if _locomotion_state == &"run" else 1.0
 	if animation != target:
 		play(target)
-
-
-func _direction_name(value: Vector2) -> StringName:
-	var angle := value.angle()
-	var octant := int(round(angle / (PI / 4.0)))
-	var direction_index := (octant + DIRECTION_NAMES.size()) % DIRECTION_NAMES.size()
-	return DIRECTION_NAMES[direction_index]
+	speed_scale = target_speed_scale
