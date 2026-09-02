267443ae551cf7454b08425b8ec1fe61cbd55d95
avarap <avarap@gmail.com>
2026-09-02 08:44:17 +0200

    feat(player): centralize run speed and 8-way orientation

--- player/player_movement.gd
+++ player/player_movement.gd
@@ -1,9 +1,33 @@
 class_name PlayerMovement
 extends RefCounted
 
+const DIRECTION_NAMES: Array[StringName] = [
+	&"e",
+	&"se",
+	&"s",
+	&"sw",
+	&"w",
+	&"nw",
+	&"n",
+	&"ne",
+]
+
 
 static func input_direction() -> Vector2:
 	return Input.get_vector("move_left", "move_right", "move_up", "move_down")
+
+
+static func speed_for_mode(base_speed: float, run_multiplier: float, running: bool) -> float:
+	return base_speed * run_multiplier if running else base_speed
+
+
+static func direction_name(value: Vector2) -> StringName:
+	if value.is_zero_approx():
+		return &"s"
+	var angle := value.angle()
+	var octant := int(round(angle / (PI / 4.0)))
+	var direction_index := (octant + DIRECTION_NAMES.size()) % DIRECTION_NAMES.size()
+	return DIRECTION_NAMES[direction_index]
 
 
 static func next_velocity(
