1e6272714e3a753dbd922cc3d4ff694a71864e79
avarap <avarap@gmail.com>
2026-09-02 02:42:05 +0200

    feat(player): drive visuals from explicit player states

--- player/player_visual.gd
+++ player/player_visual.gd
@@ -1,35 +1,29 @@
 class_name PlayerVisual
 extends AnimatedSprite2D
 
-const MOVE_EPSILON := 1.0
-const DIRECTION_NAMES: Array[StringName] = [
-	&"e",
-	&"se",
-	&"s",
-	&"sw",
-	&"w",
-	&"nw",
-	&"n",
-	&"ne",
-]
-
-var _facing := &"s"
+const RUN_ANIMATION_SPEED_SCALE := 1.45
 
 
 func _process(_delta: float) -> void:
-	var actor := get_parent() as CharacterBody2D
+	var actor := get_parent() as PlayerController
 	if actor == null:
 		return
-	var moving := actor.velocity.length_squared() > MOVE_EPSILON
-	if moving:
-		_facing = _direction_name(actor.velocity)
-	var target := StringName("%s_%s" % ["walk" if moving else "idle", _facing])
+
+	var facing := actor.get_facing_name()
+	var animation_prefix := &"idle"
+	speed_scale = 1.0
+
+	match actor.get_state():
+		PlayerController.State.WALK:
+			animation_prefix = &"walk"
+		PlayerController.State.RUN:
+			animation_prefix = &"walk"
+			speed_scale = RUN_ANIMATION_SPEED_SCALE
+		PlayerController.State.INTERACT:
+			animation_prefix = &"idle"
+		_:
+			animation_prefix = &"idle"
+
+	var target := StringName("%s_%s" % [animation_prefix, facing])
 	if animation != target:
 		play(target)
-
-
-func _direction_name(value: Vector2) -> StringName:
-	var angle := value.angle()
-	var octant := int(round(angle / (PI / 4.0)))
-	var direction_index := (octant + DIRECTION_NAMES.size()) % DIRECTION_NAMES.size()
-	return DIRECTION_NAMES[direction_index]
